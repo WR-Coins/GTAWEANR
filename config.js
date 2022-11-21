25a3c83b12a310cb55dde027c16a790762a8c9f1
WR-Coins <116741957+WR-Coins@users.noreply.github.com>
2022-11-21 17:26:50 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+/*!
+ * Quara Theme by OllieJW (https://olliejw.me)
+ * License - https://olliejw.me/tos
+ */
+
+var server_name = "GTAWEANR.GA";
+var server_ip = "GTAWEANR.GA";
+var server_port = 25568;
+var ip_copied = "Đã sao chép IP";
+
+var firefly_count = 20;
